0ee805adfed5513f001c0b15176fc17564aded17
p2101274 <jeremie.vernay@etu.univ-lyon1.fr>
2022-05-05 10:57:19 +0200

    Création attribut et métho de TRieu et TRoute

--- src/saegraphmap/linkedlist/TLieu.java
+++ src/saegraphmap/linkedlist/TLieu.java
@@ -1,14 +1,45 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package saegraphmap.linkedlist;
+public class TLIEU {
+    private String nomLieu;
+    private char type; //V (ville), R (restaurant) ou L (lieu de loisir)
+    private TLIEU precedent;
+    private TLIEU suivant;
+    private TROUTE tetelisteroutes;
 
-/**
- *
- * @author p2105876
- */
-public class TLieu {
-    
+    public TLIEU(String nomLieu, char type, TLIEU suivant, TLIEU precedent, TROUTE tetelisteroutes) {
+        this.nomLieu = nomLieu;
+        this.type = type;
+        this.suivant = suivant;
+        this.precedent = precedent;
+        this.tetelisteroutes = tetelisteroutes;
+    }
+
+    public TLIEU() {
+        this.nomLieu = null;
+        this.type = 'z';
+        this.suivant = null;
+        this.precedent = null;
+        this.tetelisteroutes = null;
+    }
+
+    public String getNomLieu() {
+        return nomLieu;
+    }
+
+    public char getType() {
+        return type;
+    }
+
+    public TLIEU getPrecedent() {
+        return precedent;
+    }
+
+    public TLIEU getSuivant() {
+        return suivant;
+    }
+
+    public TROUTE getTetelisteroutes() {
+        return tetelisteroutes;
+    }
+
+
 }
--- src/saegraphmap/linkedlist/TRoute.java
+++ src/saegraphmap/linkedlist/TRoute.java
@@ -1,14 +1,60 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package saegraphmap.linkedlist;
-
 /**
  *
  * @author p2105876
  */
-public class TRoute {
-    
+public class TROUTE {
+    private int distance;
+    private char typeRoute;//N (nationnale), D (departementale) ou A (autoroute)
+    private TLIEU lieuRejoint1;
+    private TLIEU lieuRejoint2;
+    private TROUTE suivant;
+
+    public TROUTE(int distance, char typeRoute, TLIEU lieuRejoint1, TLIEU lieuRejoint2, TROUTE suivant) {
+        this.distance = distance;
+        this.typeRoute = typeRoute;
+        this.lieuRejoint1 = lieuRejoint1;
+        this.lieuRejoint2 = lieuRejoint2;
+        this.suivant = suivant;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public char getTypeRoute() {
+        return typeRoute;
+    }
+
+    public TLIEU getLieuRejoint1() {
+        return lieuRejoint1;
+    }
+
+    public TLIEU getLieuRejoint2() {
+        return lieuRejoint2;
+    }
+
+    public TROUTE getSuivant() {
+        return suivant;
+    }
+
+    public void setDistance(int distance) {
+        this.distance = distance;
+    }
+
+    public void setTypeRoute(char typeRoute) {
+        this.typeRoute = typeRoute;
+    }
+
+    public void setLieuRejoint1(TLIEU lieuRejoint1) {
+        this.lieuRejoint1 = lieuRejoint1;
+    }
+
+    public void setLieuRejoint2(TLIEU lieuRejoint2) {
+        this.lieuRejoint2 = lieuRejoint2;
+    }
+
+    public void setSuivant(TROUTE suivant) {
+        this.suivant = suivant;
+    }
+
 }
